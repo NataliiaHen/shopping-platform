53b0e43ce74c6574fd5386ad5f4d577e73ae17eb
Nataliia Henda <nataliehenda1296@gmail.com>
2026-07-13 18:59:44 +0300

    Add basic layout

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Capsule";
+export const APP_DESCRIPTION =  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A simple e-commerce platform";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
