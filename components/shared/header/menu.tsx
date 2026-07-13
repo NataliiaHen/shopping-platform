64a1b5a1ab296e45511da8537f062271e23ff588
Nataliia Henda <nataliehenda1296@gmail.com>
2026-07-13 19:20:36 +0300

    Implement responsive menu

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,61 @@
+import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+  SheetDescription,
+} from '@/components/ui/sheet';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import ModeToggle from './mode-toggle';
+
+const Menu = () => {
+  return (
+    <div className='space-x-2'>
+      <div className='flex justify-end gap-3'>
+        <nav className='hidden md:flex w-full max-w-xs gap-1'>
+          <ModeToggle />
+          <Button asChild variant='ghost'>
+            <Link href='/cart'>
+              <ShoppingCart />
+              Cart
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link href='/sign-in'>
+              <UserIcon />
+              Sign In
+            </Link>
+          </Button>
+        </nav>
+        <nav className='md:hidden'>
+          <Sheet>
+            <SheetTrigger className='align-middle'>
+              <EllipsisVertical />
+            </SheetTrigger>
+            <SheetContent className='flex flex-col items-start bg-white'>
+              <SheetTitle>Menu</SheetTitle>
+              <ModeToggle />
+              <Button asChild variant='ghost'>
+                <Link href='/cart'>
+                  <ShoppingCart />
+                  Cart
+                </Link>
+              </Button>
+              <Button asChild>
+                <Link href='/sign-in'>
+                  <UserIcon />
+                  Sign In
+                </Link>
+              </Button>
+              <SheetDescription></SheetDescription>
+            </SheetContent>
+          </Sheet>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
