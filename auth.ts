d620fb075b88a3d10e1bf35839c9c4b3c98ff219
Nataliia Henda <nataliehenda1296@gmail.com>
2026-07-19 01:27:17 +0300

    add shipping address page

--- auth.ts
+++ auth.ts
@@ -69,6 +69,7 @@ export const config = {
       // Assign user fields fields to the token
 
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
         if (user.name === 'NO_NAME') {
@@ -81,11 +82,51 @@ export const config = {
             data: { name: token.name },
           });
         }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Overwrite any existing user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign the guest cart to the logged-in user
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
       }
 
       return token;
     },
     authorized({ request, auth }: any) {
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      const { pathname } = request.nextUrl;
+
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) {
+        return false;
+      }
+
       // Check for cart cookie
       if (!request.cookies.get('sessionCartId')) {
         // Generate cart cookie
